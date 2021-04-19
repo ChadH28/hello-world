79d73fc460772bf597b944205e13d9435a4bde7c
Chad <61545754+ChadH28@users.noreply.github.com>
2021-04-19 14:17:56 +0200

    Worked on click modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"><img> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
   <h1>{{title}}</h1>
   <input type="text" ref="name">
   <button @click="handleClick">click me</button>
   <p id="empty">
-    <Modal/>
-
   </p>
+  <div v-if="showModal">
+    <Modal 
+      :header='header'
+      :text='text' 
+      theme='dark'
+      @close="toggleModal"
+    />
+  </div>
+  <button @click.right="toggleModal">open modal</button>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import Modal from './components/Modal.vue';
 
+
 export default {
   name: 'App',
   data() {
     return {
-      title: 'Welcome to your Vue'
+      title: 'Welcome to your Vue',
+      header: 'Sign up or head out',
+      text: 'Your name here',
+      showModal: false
     }
   },
   components: {
@@ -31,10 +42,14 @@ export default {
       this.$refs.name.classList.add('active')
       this.$refs.name.focus();
       this.title.classList.add('active')
+    },
+    toggleModal() {
+      this.showModal = !this.showModal 
     }
   }
 }
 </script>
+
 
 <style>
 #app {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-        modal content
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{dark: theme === 'dark'}">
+       <h1>{{header}}</h1>
+       <p>{{text}}</p>
     </div>
   </div>
 </template>
 
+<script>
 
-<style>
+export default { 
+  props: [ 
+    'header',
+    'text', 
+    'theme'
+  ],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
 .modal {
   width: 400px;
   padding: 20px;
@@ -23,4 +41,23 @@
   width: 100%;
   height: 100%;
 }
+
+h1{
+  color: tomato; 
+}
+
+.modal.dark {
+  color:white;
+  background: rgba(255, 0, 0,0.6);
+}
+
+.modal.dark {
+  color:white;
+  background: rgba(8, 0, 0, 0.6);
+}
+
+.modal.dark h1 {
+  color:white;
+}
+
 </style>
